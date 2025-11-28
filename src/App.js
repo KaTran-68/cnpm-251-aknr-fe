72d4877aa2df21416eee42d1aaa6e2a0ef1151c1
qhuuwx <phanquochuuwx@gmail.com>
2025-11-28 15:33:14 +0700

    Remove header footer in App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import './App.css';
 import AppRoutes from './routes/AppRoutes';
-import Header from './components/Common/Header/Header';
-import Footer from './components/Common/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
-      <Header />
       <main className="App-body">
         <AppRoutes />
       </main>
-      <Footer />
     </div>
   );
 }
